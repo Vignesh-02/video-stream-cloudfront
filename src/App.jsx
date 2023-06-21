2bedf88f9764cfc7795c581415add5772713b9be
Vignesh-02 <laserlikefocus000@gmail.com>
2023-06-21 18:13:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="App">
+            <h2>Bienvenidos to Vigya's video streaming site</h2>
+            <video width="700px" height="480px" controls>
+                    <source src="https://d1zxp7wqjt19o.cloudfront.net/gone.mp4" type="video/mp4"/>
+            </video>
+        </div>
+    
+    </>
+  )
+}
+
+export default App
